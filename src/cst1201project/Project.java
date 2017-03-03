41f263c97609eed5f74886ec1bdf26e3be57cabb
KajolThapa <Kajoolthapa@hotmail.com>
2017-03-02 22:00:33 -0500

    First Commit CoffeeMenu

---
+++ src/cst1201project/Project.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cst1201project;
+
+
+
+/**
+ *
+ * @author Kajol Thapa
+ */
+public class Project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+      new MainMenu().setVisible(true);   // TODO code application logic here
+    }
+    
+}
